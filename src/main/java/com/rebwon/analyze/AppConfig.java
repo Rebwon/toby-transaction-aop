b2db76a6124811a77e4ec715018fb9c345f86c21
Rebwon <msolo021015@naver.com>
2021-08-08 19:31:01 +0900

    Using ProxyBeanFactory

--- src/main/java/com/rebwon/analyze/AppConfig.java
+++ src/main/java/com/rebwon/analyze/AppConfig.java
@@ -1,6 +1,11 @@
 package com.rebwon.analyze;
 
 import javax.sql.DataSource;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -18,14 +23,28 @@ public class AppConfig {
     }
 
     @Bean
+    public TransactionAdvice transactionAdvice() {
+        return new TransactionAdvice(platformTransactionManager());
+    }
+
+    @Bean
+    public Pointcut transactionPointcut() {
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.addMethodName("upgrade*");
+        return pointcut;
+    }
+
+    @Bean
+    public Advisor transactionAdvisor() {
+        return new DefaultPointcutAdvisor(transactionPointcut(), transactionAdvice());
+    }
+
+    @Bean
     public UserService userService(UserDao userDao) throws Exception {
-        JdkDynamicProxyBeanFactory beanFactory = new JdkDynamicProxyBeanFactory(
-            new UserServiceImpl(userDao),
-            platformTransactionManager(),
-            "",
-            UserService.class
-        );
-        return (UserService) beanFactory.getObject();
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.setTarget(new UserServiceImpl(userDao));
+        proxyFactoryBean.addAdvisor(transactionAdvisor());
+        return (UserService) proxyFactoryBean.getObject();
     }
 
 //    @Bean
